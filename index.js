d63d714af41d71a094cc53716dd1b17c11f727cf
Alysha Neeamuth <alyshanee@gmail.com>
2018-07-25 18:26:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  
+  return drivers.map(function(lowerCaseName){
+    
+    return lowerCaseName.toLowerCase() });
+}
+
+function nameToAttributes(firstName, lastName){
+  
+}
+
+
+let students = [harryPotter, ronWeasley, hermioneGranger, ginevraWeasley];
+let patroni = students.map( student => student.namePatronus() )
+
+
+
+
+
